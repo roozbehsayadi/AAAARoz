60b72e69a22b143d7e7babcf736ba57f945c020c
roozbehsayadi <roozbehsayadi@gmail.com>
2018-04-23 13:37:32 +0430

    print method added!

--- src/AAA.java
+++ src/AAA.java
@@ -2,4 +2,8 @@ public class AAA {
     public static void main(String[] args) {
         System.out.println("HEllo Git!");
     }
+
+    public static void print(){
+        System.out.println( "PRint!" );
+    }
 }
